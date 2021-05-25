eb260d07bf600b8af0e978a481738879534d83c6
dDmioko <aleksejj.anisimov@gmail.com>
2021-05-26 01:11:48 +0700

    Add camera & cube

---
+++ Assets/GameResources/Scripts/MovementScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    [SerializeField]
+    private float MoveSpeed = 1f;
+
+    private float horizontal;
+    private float vertical;
+
+    private void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        float x = transform.position.x + (horizontal * MoveSpeed);
+        float z = transform.position.z + (vertical * MoveSpeed);
+        transform.position = new Vector3(x, transform.position.y, z);
+    }
+}
