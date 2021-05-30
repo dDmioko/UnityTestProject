e180187d47c02e3b17546363811c9fd4f4098d2a
dDmioko <aleksejj.anisimov@gmail.com>
2021-05-30 21:11:47 +0700

    Add rebinding

---
+++ Assets/GameResources/Scripts/Rebinding.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Linq;
+using Extentions;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using static UnityEngine.InputSystem.InputActionRebindingExtensions;
+
+public class Rebinding : MonoBehaviour
+{
+    [SerializeField] private InputActionReference inputAction;
+    [SerializeField] private Directions direction = Directions.Up;
+    [SerializeField] private Text bindText;
+    [SerializeField] private Text actionText;
+
+    private void OnEnable()
+    {
+        UpdateBindingText();
+    }
+
+    public void Rebind()
+    {
+        inputAction.action.PerformInteractiveRebinding((int)direction + 1)
+            .OnMatchWaitForAnother(0.1f)
+            .OnComplete(operation =>
+            {
+                RebindComplete(operation);
+                UpdateBindingText();
+            })
+            .Start();
+    }
+
+    private void RebindComplete(RebindingOperation operation)
+    {
+        Debug.Log(operation.action.GetBindingDisplayString());
+    }
+
+    private void UpdateBindingText()
+    {
+        string name = inputAction.action.name;
+        InputBinding binding = inputAction.action.bindings.Where(p => p.action == name && p.isPartOfComposite).ToList()[(int)direction];
+
+        bindText.text = InputControlPath.ToHumanReadableString(
+            binding.effectivePath,
+            InputControlPath.HumanReadableStringOptions.OmitDevice | InputControlPath.HumanReadableStringOptions.UseShortNames);
+        actionText.text = binding.name.FirstCharToUpper();
+    }
+
+    private enum Directions
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+}
